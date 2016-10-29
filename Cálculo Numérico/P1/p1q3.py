cc3c138b12a64a3216b440a066c0a93182dedbe1
masbicudo <masbicudo@gmail.com>
2016-10-29 03:28:40 -0200

    P1 Cálc. Numérico q3 - funcional - falta ainda inputs de usuário?

--- Cálculo Numérico/P1/p1q3.py
+++ Cálculo Numérico/P1/p1q3.py
@@ -4,48 +4,85 @@ Created on Sat Oct 15 16:09:04 2016
 """
 
 import sys as sys
-import math as math
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import cm as cm
 import scipy.linalg as la
 
+ambiente1 = [
+    5.0,  # L: comprimento do fio
+    0.0,  # a: posição do início do fio
+    2.0,  # tf: duração temporal simulada
+    
+    '20.0 if 2 < x < 4 else 10.0', # f(x)
+    
+    '0.0', # M(t): temperatura do meio ambiente
+    
+    '-10.0', # h(x): func. de contorno esquerda
+    1.0, # alpha: coef. de T no lado esquerdo
+    -1.0, # rho:   coef. de dT no lado esquerdo
+    
+    '10.0', # g(x): func. de contorno direita
+    1.0, # beta:  coef. de T no lado direito
+    1.0, # sigma: coef. de dt no lado direito
+    
+    1.0,  # p: difusividade térmica
+    -1.0, # q: coef. de absorção da temp. da barra pelo meio ambiente
+    -1.0, # r: coef. de geração de temp.
+    
+    25, # Nx: número de discretizações espaciais
+    31, # Nt: número de discretizações temporais
+    ]
 
-L = 5.0
-a = 0.0
-tf = 2.0
+custom = [
+    5.0,  # comprimento do fio
+    0.0,  # posição do início do fio
+    2.0, # duração temporal simulada
+    
+    '20.0 if 2 < x < 4 else 10.0', # f(x)
+    
+    '5.0', # M(t) - temperatura do meio ambiente
+    
+    '0.0', # h(x)
+    1.0, # coef. de T no lado esquerdo
+    0.1,   # coef. de dT no lado esquerdo
+    
+    '0.0', # g(x)
+    1.0,  # coef. de T no lado direito
+    0.1, # coef. de dt no lado direito
+    
+    1.0,  # difusividade térmica
+    -1.0, # coef. de absorção da temp. da barra pelo meio ambiente
+    -1.0, # coef. de geração de temp.
+    
+    25, # número de discretizações espaciais
+    30, # número de discretizações temporais
+    ]
 
-f = lambda x: 1.0 if 2 < x < 4 else 0.0
-h = lambda x: 0.5
-g = lambda x: 0.5
 
-alpha = 1.0
-ro = 0.1
-beta = 1.0
-sigma = 0.1
-
-p = 1
-q = 0
-r = 0
-
-Nx = 25
-Nt = 30
+L,a,tf,f,M,h,alpha,rho,g,beta,sigma,p,q,r,Nx,Nt = ambiente1
+f = eval('lambda x:'+f)
+h = eval('lambda x:'+h)
+g = eval('lambda x:'+g)
+M = eval('lambda t:'+M)
 
 
+# criando algumas constantes que serão necessárias
 dx = L/Nx
 dt = tf/Nt
 fn = lambda n: f(a + n*dx)
 Nx1 = Nx + 1
 
-if abs(alpha*dx - ro) < 1e-7:
-    sys.exit('alpha*dx - ro == 0: {0}*{1} - {2}'.format(alpha, dx, ro))
+if abs(alpha*dx - rho) < 1e-7:
+    sys.exit('alpha*dx - rho == 0: {0}*{1} - {2}'.format(alpha, dx, rho))
 
 
+paleta = 'afmhot'
 
 T = []
 T0 = []
 T.append(T0)
-T0.append((dx*h(0) - ro*fn(1))/(alpha*dx - ro))
+T0.append((dx*h(0) - rho*fn(1))/(alpha*dx - rho))
 for n in xrange(1, Nx1):
     T0.append(fn(n))
 T0.append((dx*g(0) + sigma*fn(Nx))/(beta*dx + sigma))
@@ -61,8 +98,9 @@ def draw(data, cmap):
     ax1.grid(True)
     plt.show()
 
-print T0
-draw(np.matrix([T0]), 'hot')
+print '--'*32
+print 'Função f(x) que define as temperaturas iniciais no fio'
+draw(np.matrix([T0]), paleta)
 
 def TT(k,i):
     return T[i][k]
@@ -78,20 +116,21 @@ for lin in xrange(1, Nx1):
         if (col == lin - 1):
             val = -p*dt
         elif (col == lin):
-            val = 2*dx*dx + 2*p*dt + q*dx*dt - r*dx*dx*dt
+            val = 2*dx*dx + 2*p*dt - q*dx*dt
         elif (col == lin + 1):
-            val = -dt*(p + q*dx)
+            val = -p*dt
         
         if (lin == 1 and col == 1):
-            val = val + (ro*p*dx)/(alpha*dx - ro)
+            val = val + (rho*p*dt)/(alpha*dx - rho)
         elif (lin == Nx and col == Nx):
-            val = val - sigma*dt*(p + q*dx)/(beta*dx + sigma)
+            val = val - (sigma*p*dt)/(beta*dx + sigma)
         
         cols.append(val)
     lines.append(cols)
 matA = np.matrix(lines)
 
-print matA.shape
+print '--'*32
+print 'Matriz A do sistema Ax=b, que será aplicada sucessivamente'
 draw(matA, cm.coolwarm)
 
 # montando o vetor resultado do sistema
@@ -99,11 +138,13 @@ draw(matA, cm.coolwarm)
 def calcNextTimeResultVec(i):
     result = []
     for k in xrange(1, Nx1):
-        res = TT(k + 1, i)*dt*(p + q*dx) - TT(k, i)*(-2*dx*dx + 2*p*dt + q*dx*dt - r*dx*dx*dt) + TT(k - 1, i)*p*dt
+        res = p*dt*(TT(k - 1, i) + TT(k + 1, i))
+        res += TT(k, i)*(2*dx*dx - 2*p*dt + q*dx*dt)
+        res -= dx*dt*(M(i*dt) + M((i+1)*dt))*(q - r*dx)
         if (k == 1):
-            res = res - (dx*h((i + 1)*dt))/(alpha*dx - ro)
+            res = res + (dx*h((i + 1)*dt)*p*dt)/(alpha*dx - rho)
         elif (k == Nx):
-            res = res - (dx*g((i + 1)*dt))/(beta*dx + sigma)
+            res = res + (dx*g((i + 1)*dt)*p*dt)/(beta*dx + sigma)
         result.append([res])
     return np.matrix(result)
 
@@ -113,14 +154,19 @@ def drawVec(data, cmap):
     ax1.imshow(data, interpolation="nearest", cmap=cmap, vmin=vmin, vmax=vmax)
     plt.show()
 
-drawVec(np.matrix([T0]), 'hot')
+#drawVec(np.matrix([T0]), paleta)
 # resolvendo iterativamente cada passo no tempo
 for t in xrange(1, Nt):
     vecB = calcNextTimeResultVec(t - 1)
     Ti = la.solve(matA, vecB)
     Ti = list(Ti.flat)
     #print Ti
-    Ti[:0] = [(dx*h(t*dt) - ro*Ti[0])/(alpha*dx - ro)]
+    Ti[:0] = [(dx*h(t*dt) - rho*Ti[0])/(alpha*dx - rho)]
     Ti.append((dx*g(t*dt) + sigma*Ti[-1])/(beta*dx + sigma))
-    drawVec(np.matrix([Ti]), 'hot')
+    #drawVec(np.matrix([Ti]), paleta)
     T.append(Ti)
+
+print '--'*32
+print 'Evolução temporal de temperaturas no fio'
+matT = np.matrix(T)
+draw(matT, paleta)
