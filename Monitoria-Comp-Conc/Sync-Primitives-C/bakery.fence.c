f938114764aa8dc947d5d974b93bd22f7dbb9408
Miguel Angelo <masbicudo@gmail.com>
2018-04-24 22:51:07 -0300

    Monitoria Comp Conc - Sync primitives in C - BUG correction: memory fences are needed in primitives

--- Monitoria-Comp-Conc/Sync-Primitives-C/bakery.fence.c
+++ Monitoria-Comp-Conc/Sync-Primitives-C/bakery.fence.c
@@ -16,6 +16,15 @@
 #define ENABLE_THREAD_0
 #define ENABLE_THREAD_1
 
+#define ENABLE_MEM_FENCE // On modern CPUs and compilers, optimizations may lead to incorrect results.
+                         // The compiler may reorder instructions, remove busy waits, and do other optimizations.
+                         // In the case of the Lamport's Bakery, the compiler can optimize consecutive
+                         // sets to the same variable with the last value.
+                         // Also, the CPU memory model, also allows for instruction and read/writes reordering,
+                         // and also have cache coherence problems.
+                         // Using memory fences will tell the CPU not to reorder anything over the fence.
+                         // Another way of guaranteing the order is using the -O3 option with gcc.
+                         // When using -O3 optimization option, memory fences are not needed.
 //#define YIELD_WAIT
 
 volatile bool Entering[NUM_THREADS];
@@ -28,6 +37,7 @@ void lock(int i);
 void unlock(int i);
 int max(volatile int* items, int count);
 int cmp(int a0, int a1, int b0, int b1);
+void mem_fence();
 void wait();
 
 int main()
@@ -69,15 +79,25 @@ void *thread(void *num)
 }
 
 void lock(int i) {
+    // Problems that can happen here, without the memory fence:
+    // - compiler removes the line that sets Entering to true, since it will be set to false anyway
+    // - reordering, by compiler or CPU of the set instructions
+    // - reordering of the memory reads/writes from/to the memory
+    // - cache coherency problem between different processors
+    // Another way of guaranteing the order is using the -O3 option with gcc
     Entering[i] = true;
+    mem_fence();
     Number[i] = 1 + max(Number, NUM_THREADS);
+    mem_fence();
     Entering[i] = false;
     for (int j = 0; j < NUM_THREADS; j++) {
         // Wait until thread j receives its number:
-        while (Entering[j]) ;
+        while (Entering[j])
+            wait();
         // Wait until all threads with smaller numbers or with the same
         // number, but with higher priority, finish their work:
-        while ((Number[j] != 0) && (cmp(Number[j], j, Number[i], i) < 0)) ;
+        while ((Number[j] != 0) && (cmp(Number[j], j, Number[i], i) < 0))
+            wait();
     }
 }
 
@@ -100,4 +120,16 @@ int cmp(int a0, int a1, int b0, int b1) {
     if (a1 < b1) return -1;
     if (a1 > b1) return +1;
     return 0;
+}
+
+void mem_fence() {
+    #ifdef ENABLE_MEM_FENCE
+    __sync_synchronize();
+    #endif
+}
+
+void wait() {
+    #ifdef YIELD_WAIT
+    sched_yield();
+    #endif
 }
--- Monitoria-Comp-Conc/Sync-Primitives-C/peterson.c
+++ Monitoria-Comp-Conc/Sync-Primitives-C/peterson.c
@@ -1,16 +1,32 @@
 // Code implemented following Wikipedia pseudocode
 // https://en.wikipedia.org/wiki/Peterson%27s_algorithm
+// With hints about memory fences from:
+// https://www.geeksforgeeks.org/petersons-algorithm-for-mutual-exclusion-set-2-cpu-cycles-and-memory-fence/
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <pthread.h>
 #include <unistd.h>
 
+#define AMOUNT 5000000
+
 #define LOCK_ENABLED
+
 #define ENABLE_THREAD_0
 #define ENABLE_THREAD_1
 
-bool flag[2] = {false, false};
+#define ENABLE_MEM_FENCE // On modern CPUs and compilers, optimizations may lead to incorrect results.
+                         // The compiler may reorder instructions, remove busy waits, and do other optimizations.
+                         // Also, the CPU memory model, also allows for instruction and read/writes reordering,
+                         // and also have cache coherence problems.
+                         // Using memory fences will tell the CPU not to reorder anything over the fence.
+//#define YIELD_WAIT
+
+// In my tests, the fastest setup was using busy wait (by undefining YIELD_WAIT)
+// and by calling __sync_synchronize() inside another method instead of directly.
+
+int flag0 = 0;
+int flag1 = 0;
 int turn;
 
 int resource = 0;
@@ -18,11 +34,14 @@ int resource = 0;
 void *thread0(void *num);
 void *thread1(void *num);
 
+void mem_fence();
+void wait();
+
 int main()
 {
     printf("Testing Peterson mutual exclusion algorithm\n");
 
-    pthread_t thread_id[2]; // each thread will simulate the behavior of one philosopher
+    pthread_t thread_id[2];
 
     #ifdef ENABLE_THREAD_0
     int num0 = 0;
@@ -40,58 +59,66 @@ int main()
     #ifdef ENABLE_THREAD_1
     pthread_join(thread_id[1], NULL);
     #endif
+
+    printf("Contagem final: %d\n", resource);
 }
 
 void *thread0(void *num)
 {
     int i = *((int *)num);
-    while (1)
+    for (int it = 0; it < AMOUNT; it++)
     {
         #ifdef LOCK_ENABLED
-        flag[0] = true;
+        flag0 = 1;
         turn = 1;
-        while (flag[1] == true && turn == 1)
-            sleep(0);
+        mem_fence();
+        while (flag1 == 1 && turn == 1)
+            wait();
         #endif
 
         // critical section
-        for (int it = 0; it < 10; it++)
-        {
-            printf("%i", i);
-            sleep(0);
-        }
-        printf("\n");
+        resource++;
         // end of critical section
 
         #ifdef LOCK_ENABLED
-        flag[0] = false;
+        flag0 = 0;
         #endif
     }
+    printf("Contagem thread0: %d\n", resource);
 }
 
 void *thread1(void *num)
 {
     int i = *((int *)num);
-    while (1)
+    for (int it = 0; it < AMOUNT; it++)
     {
         #ifdef LOCK_ENABLED
-        flag[1] = true;
+        flag1 = 1;
         turn = 0;
-        while (flag[0] == true && turn == 0)
-            sleep(0);
+        mem_fence();
+        while (flag0 == 1 && turn == 0)
+            wait();
         #endif
 
         // critical section
-        for (int it = 0; it < 10; it++)
-        {
-            printf("%i", i);
-            sleep(0);
-        }
-        printf("\n");
+        resource++;
         // end of critical section
 
         #ifdef LOCK_ENABLED
-        flag[1] = false;
+        flag1 = 0;
         #endif
     }
+    printf("Contagem thread1: %d\n", resource);
+}
+
+void mem_fence() {
+    #ifdef ENABLE_MEM_FENCE
+    __sync_synchronize();
+    #endif
+}
+
+void wait() {
+    #ifdef YIELD_WAIT
+    sched_yield();
+    #endif
 }
