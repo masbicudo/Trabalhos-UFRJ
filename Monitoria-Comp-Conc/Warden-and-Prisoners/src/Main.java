ec60aac183a13c9ad7773fa0190fb24d441e3dbe
Miguel Angelo <masbicudo@gmail.com>
2018-05-07 17:10:06 -0300

    Monitoria Comp. Conc. - Warden and Prisoners - get number of inmates from CLI

--- Monitoria-Comp-Conc/Warden-and-Prisoners/src/Main.java
+++ Monitoria-Comp-Conc/Warden-and-Prisoners/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
-    private static final int numPrisoners = 5;
+    private static int numPrisoners = 5;
 
     public static void main(String[] args) throws Exception {
+        for(int it = 0; it < args.length; it++)
+            System.out.println(args[it]);
+
+        if (args.length >= 1)
+            numPrisoners = Integer.parseInt(args[0]);
+
         Prisoner[] prisoners = new Prisoner[numPrisoners];
         Room room = new Room();
         Warden warden = new Warden(room, prisoners);
