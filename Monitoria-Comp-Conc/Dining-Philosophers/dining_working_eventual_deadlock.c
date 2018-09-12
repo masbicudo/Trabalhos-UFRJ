816bb99801bca27e584ee5c983ba41ef95499942
Miguel Angelo <masbicudo@gmail.com>
2018-09-12 00:25:54 -0300

    Monitoria Comp. Conc.: typos and docs - how to compile

--- Monitoria-Comp-Conc/Dining-Philosophers/dining_working_eventual_deadlock.c
+++ Monitoria-Comp-Conc/Dining-Philosophers/dining_working_eventual_deadlock.c
@@ -106,7 +106,7 @@ void take_chopstick(int ph_num)
 
 void put_chopstick(int ph_num)
 {
-    printf("Philosopher %d putting chopstick %d and %d down\n", ph_num + 1, LEFT + 1, ph_num + 1);
+    printf("Philosopher %d putting chopstick %d and %d down\n", ph_num + 1, LEFT + 1, RIGHT + 1);
     printf("Philosopher %d is thinking\n", ph_num + 1);
     sem_post(&chopsticks[LEFT]);
     sleep(1);
